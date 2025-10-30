badc3b3aecba5c352cc5a58d74e63ef013b40fd0
Quan <ntminhquan2106@gmail.com>
2025-10-29 21:34:16 -0700

    switch to env variable for Firebase credentails

--- backend/mobile-leetnote-backend/src/main/java/com/example/leetnote_backend/config/FirebaseConfig.java
+++ backend/mobile-leetnote-backend/src/main/java/com/example/leetnote_backend/config/FirebaseConfig.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -15,7 +16,8 @@ public class FirebaseConfig {
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
 
-        InputStream firebaseConfigStream = this.getClass().getClassLoader().getResourceAsStream("leetnote-app-7f481-firebase-adminsdk.json");
+        String firebasePath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+        InputStream firebaseConfigStream = new FileInputStream(firebasePath);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(firebaseConfigStream))
