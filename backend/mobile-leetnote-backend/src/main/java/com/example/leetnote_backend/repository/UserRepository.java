aa36bad5eaa2f764be4231eaac6525c567043030
Quan <ntminhquan2106@gmail.com>
2025-11-11 02:50:18 -0700

    remove annotation

--- backend/mobile-leetnote-backend/src/main/java/com/example/leetnote_backend/repository/UserRepository.java
+++ backend/mobile-leetnote-backend/src/main/java/com/example/leetnote_backend/repository/UserRepository.java
@@ -2,11 +2,9 @@ package com.example.leetnote_backend.repository;
 
 import com.example.leetnote_backend.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByFirebaseUid(String firebaseUid);
     Optional<User> findByEmail(String email);
